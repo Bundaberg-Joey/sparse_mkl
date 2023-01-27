c53289c20fb05b7c99e9af00a7ce7a376cdb4177
Bundaberg-Joey <calumhand94@hotmail.com>
2023-01-27 10:55:34 +0000

    adding tanimoto kernel tests from fpa repo

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,7 @@ from sklearn.datasets import make_regression
 
 from mkl.acquisition import GreedyNRanking
 from mkl.data import Hdf5Dataset
-from mkl.kernel import RbfKernelIdx, WhiteKernelIdx
+from mkl.kernel import RbfKernelIdx, WhiteKernelIdx, TanimotoKernelIdx
 
 
 # -----------------------------------------------------------------------------------------------------------------------------
@@ -143,5 +143,55 @@ def test_WhiteKernelIdx(n, m):
         
     with pytest.raises(IndexError):
         idx_kernel([len(ref)+1])
+
+
+def test_TanimotoKernelIdx__calc_sim():
+    X = np.array([[1, 0, 0, 1], [1, 0, 1, 0], [1, 0, 1, 1]]).astype(bool)  # convert to silence sklearn warning
+    out = TanimotoKernelIdx._calc_sim(X, X, n_jobs=1)
+
+    assert all(np.diag(out) == 1), 'diagonal should be 1'
+    assert out.shape == (X.shape[0], X.shape[0]), 'should be square matrix w.r.t number of entries in X.'
+
+    expected = np.array([[1., 0.33333333, 0.66666667],
+                         [0.33333333, 1., 0.66666667],
+                         [0.66666667, 0.66666667, 1.]])
+
+    assert abs((out - expected)).sum() < 1e-7, 'Arrays should match, difference here due to floating point precision.'
+
+
+@pytest.mark.parametrize("x, y, eval_gradient", 
+[
+    (np.array([0, 1, 4, 5]), None, False),
+    (np.array([0, 1, 4, 5]).reshape(-1, 1), None, False),
+    (np.array([0, 1, 4, 5]), None, True),
+    (np.array([0, 1, 4, 5]).reshape(-1, 1), None, True),
+    (np.array([0, 1, 4, 5]), np.arange(10), False),
+    (np.arange(10), np.array([0, 1, 4, 5]), False),
+    (np.arange(10), np.array([0, 1, 4, 5]).reshape(-1, 1), False),
+    (np.arange(10).reshape(-1, 1), np.array([0, 1, 4, 5]).reshape(-1, 1), True),
+    (np.arange(10), None, False),
+    (np.arange(10).reshape(-1, 1), None, True)
+])
+def test_DynamicTanimotoKernel___call__(x, y, eval_gradient):
+    fm = np.random.randint(0, 2, size=(30, 5), dtype=int)
+    kernel = TanimotoKernelIdx(fm, 1)
+    out = kernel(x, y, eval_gradient)
+
+    if eval_gradient:
+        covar, grad = out
+        assert grad.shape == (len(x), len(x), 0)
+        assert grad.sum() == 0
+    else:
+        covar = np.array(out)
+
+    xr = x.ravel()
+    yr = y.ravel() if y is not None else xr
+
+    assert covar.ndim == 2
+    assert covar.shape == (len(xr), len(yr))
+
+    ref = kernel._calc_sim(fm[xr], fm[yr], 1)
+
+    np.testing.assert_allclose(covar, ref)
 
 # -----------------------------------------------------------------------------------------------------------------------------
