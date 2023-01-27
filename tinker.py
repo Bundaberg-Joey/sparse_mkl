3f26dfb60dbf9009333353f4379222695bf9cf11
Bundaberg-Joey <calumhand94@hotmail.com>
2023-01-27 09:43:52 +0000

    tinker file

--- tinker.py
+++ tinker.py
@@ -13,11 +13,14 @@ internal_model = GaussianProcessRegressor(kernel=kernel, normalize_y=True)
 model = SparseGaussianProcess(internal_model, X_inducing=X_ind)
 
 # about 5-8 seconds to fit to 1_000 and sample for 100_000 and inducing matrix of 100
-#model.fit(X[:1000], y[:1000])
-#post = model.sample_y(X, n_samples=100)
+model.fit(X[:1000], y[:1000])
+post = model.sample_y(X, n_samples=100)
 
 # crashes after a minute because of memory issues, THE CODE WORKS WOOHOOO !!!!!
-internal_model.fit(X[:1000], y[:1000])  
-post = internal_model.sample_y(X)
+#internal_model.fit(X[:1000], y[:1000])  
+#post = internal_model.sample_y(X)
 
 print(post.shape)
+print(np.isnan(post).any())
+print(np.isinf(post).any())
+
