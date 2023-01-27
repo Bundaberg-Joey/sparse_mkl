70b61d750a173f41f6297dfd845de441575b006b
Bundaberg-Joey <calumhand94@hotmail.com>
2023-01-27 09:54:08 +0000

    tests for Hdf5Dataset

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 
 from mkl.acquisition import GreedyNRanking
+from mkl.data import Hdf5Dataset
 
 
 # -----------------------------------------------------------------------------------------------------------------------------
@@ -35,4 +36,25 @@ def test_GreedyNRanking(n_samp, n_opt):
     
 # -----------------------------------------------------------------------------------------------------------------------------
 
+
+@pytest.mark.parametrize("indices", 
+                         [
+                             [0, 5, 2, 10],
+                            [[0], [5], [2], [10]],
+                             [19, 18, 17],
+                             list(range(10)),
+                             [1]
+                         ])
+def test_Hdf5Dataset(indices):
+    m = 5
+    ref = np.arange(100).reshape(20, m)
     
+    dataset = Hdf5Dataset('tests/test.hdf5')
+    out = dataset[indices]
+    
+    assert out.ndim == 2
+    assert out.shape == (len(indices), m)
+    assert np.array_equal(out, ref[np.ravel(indices)])
+    
+    
+# -----------------------------------------------------------------------------------------------------------------------------
