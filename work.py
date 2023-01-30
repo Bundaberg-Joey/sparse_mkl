ca0220c33a81e5024a69749b6507ba0fddad0e33
Bundaberg-Joey <calumhand94@hotmail.com>
2023-01-30 13:16:36 +0000

    prospector uses fitted kernel to determine sigma matrices

--- work.py
+++ work.py
@@ -218,10 +218,10 @@ class Prospector:
             # dragons...
             # email james.l.hook@gmail.com if this bit goes wrong!
             print('fitting sparse model')
-            DXM = euclidean_distances(np.divide(X, self.l), np.divide(self.M, self.l), squared=True)
-            self.SIG_XM = self.a * np.exp(-DXM / 2)
-            DMM = euclidean_distances(np.divide(self.M, self.l), np.divide(self.M, self.l), squared=True)
-            self.SIG_MM = self.a * np.exp(-DMM / 2) + np.identity(self.M.shape[0]) * self.lam * self.a
+
+            self.SIG_XM = self.GP.kern.K(X, self.M)
+            self.SIG_MM = self.GP.kern.K(self.M, self.M) + (np.identity(self.M.shape[0]) * self.lam * self.a)
+ 
             self.B = self.a + self.b - np.sum(np.multiply(np.linalg.solve(self.SIG_MM, self.SIG_XM.T), self.SIG_XM.T),0)
             K = np.matmul(self.SIG_XM[tested].T, np.divide(self.SIG_XM[tested], self.B[tested].reshape(-1, 1)))
             self.SIG_MM_pos = self.SIG_MM - K + np.matmul(K, np.linalg.solve(K + self.SIG_MM, K))
