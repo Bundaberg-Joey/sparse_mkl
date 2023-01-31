447f9b35dae4819bbc8bcb029dc1c7fc04d50646
Bundaberg-Joey <calumhand94@hotmail.com>
2023-01-31 18:03:26 +0000

    adding MKL dense model (weights currently fixed)

--- mkl/model.py
+++ mkl/model.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 import numpy as np
 from numpy.typing import NDArray
 
@@ -64,6 +64,61 @@ class DenseTanimotoModel(DenseGpflowModel):
             mean_function=gpflow.mean_functions.Constant()
         )
         return model
+    
+    
+class DenseMultipleKernelLearner(DenseTanimotoModel):
+    
+    def __init__(self, X: List, X_M: List):  # each can have their own inducing matrix!!
+        self.X = X  #list of data objects
+        self.M = [xi[xm] for xi, xm in zip(self.X, X_M)]  # X_m list of indices (one per kernel) self.M is list of data objects
+        self.n_kernels = len(self.X)
+        self.weights = np.ones(self.n_kernels) / self.n_kernels  # equal weights
+        self.models = []
+        
+    def fit(self, X_ind, y_val):
+        
+        self.models = []
+                
+        for i in range(self.n_kernels):
+            self.M[i] = np.vstack((self.X[i][X_ind], self.M[i]))  # update each inducing matrix with training indices
+            
+            x_ = self.X[i][X_ind]
+            y_ = np.reshape(y_val, (-1, 1))
+            model = self.build_model(x_, y_)
+            
+            opt = gpflow.optimizers.Scipy()
+            opt.minimize(model.training_loss, model.trainable_variables)  
+            self.models.append(model)
+                        
+    def get_prior_mu(self):
+        return sum([w * m.mean_function.c.numpy() for w, m in zip(self.weights, self.models)])
+
+    def get_kernel_var(self):
+        return sum([w * m.kernel.variance.numpy() for w, m in zip(self.weights, self.models)])
+    
+    def get_gaussian_var(self):
+        return sum([w * m.likelihood.variance.numpy() for w, m in zip(self.weights, self.models)])
+
+    def calc_k_xm(self):
+        k = self._calc_weighted_k(self.X, self.M)
+        print(F'k_xm={k.shape}')
+        return k
+            
+    def calc_k_mm(self):
+        k = self._calc_weighted_k(self.M, self.M)
+        print(F'k_mm={k.shape}')
+        return k
+            
+
+    def _calc_weighted_k(self, A, B):
+        k = []
+        
+        for i in range(self.n_kernels):
+            k_ = self.models[i].kernel.K(A[i], B[i]).numpy()
+            k.append(k_ * self.weights[i])
+            
+        return sum(k)
+
       
 
 # ------------------------------------------------------------------------------------------------------------------------------------
