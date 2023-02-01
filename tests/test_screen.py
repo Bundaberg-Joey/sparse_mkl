2b59bf6674d32551c6f23eedea29a7b993c4c210
Bundaberg-Joey <calumhand94@hotmail.com>
2023-02-01 11:59:53 +0000

    adding some simple HDF5 screenings to make sure prediction / acquisition work well together with dataset

--- tests/test_screen.py
+++ tests/test_screen.py
@@ -9,6 +9,7 @@ from scipy.cluster.vq import vq
 from mkl.sparse import SparseGaussianProcess
 from mkl.dense import DenseRBFModel, DenseTanimotoModel, DenseMultipleKernelLearner, DynamicDenseMKL
 from mkl.acquisition import GreedyNRanking
+from mkl.data import Hdf5Dataset
 
 
 # -----------------------------------------------------------------------------------------------------------------------------
@@ -174,5 +175,106 @@ def test_sparse_gp_screening_dense_mkl_models_double_same_kernel(model):
         
     assert n_top_found >= 17  # average with random guessing is 6.5
     
+
+# -----------------------------------------------------------------------------------------------------------------------------
+
+
+@pytest.mark.slow
+def test_sparse_gp_screening_hdf5():
+    X = Hdf5Dataset('tests/data/COF_p.hdf5', 'X')
+    y = Hdf5Dataset('tests/data/COF_p.hdf5', 'y')[:].ravel()
+
+    n_top = 100
+    y_top = np.argsort(y)[-n_top:]
+
+    # ----------------------------------------------------
+    X_train_ind = list(RAND.choice(len(X), size=50))
+    y_train = y[X_train_ind]
+
+
+    # ----------------------------------------------------
+    kms = KMeans(n_clusters=300, max_iter=5)
+    kms.fit(X[:])
+    X_cls = kms.cluster_centers_
+    cls_ind, _ = vq(X_cls, X[:])  # need to use in data locations
+
+    # ----------------------------------------------------
+
+
+    dense_model = DenseRBFModel(X=X, X_M=cls_ind)
+    model = SparseGaussianProcess(dense_model=dense_model)
+    acqu = GreedyNRanking()
+
+    for itr in range(50, 100):
+            
+        y_train = y[X_train_ind]
+
+        if itr % 10 == 0:
+            model.update_parameters(X_train_ind, y_train)
+        model.update_data(X_train_ind, y_train)
+
+        posterior = model.sample_y(n_samples=1)
+        _, _  = model.predict()
+        alpha = acqu.score_points(posterior)
+        alpha_ranked = np.argsort(alpha)[::-1]
+        to_sample = [i for i in alpha_ranked if i not in X_train_ind][0]
+        
+        X_train_ind.append(to_sample)    
+        n_top_found = sum([1 for i in X_train_ind if i in y_top])
+        
+        print(F'[{itr} / 100] : {n_top_found}/{n_top} found : y_max_tested={max(y):.3f} : y_sampled={y[to_sample]}')
+        
+        
+    assert n_top_found >= 25
+
+
+# -----------------------------------------------------------------------------------------------------------------------------
+
+
+@pytest.mark.slow
+def test_sparse_gp_screening_tanimoto_hdf5():
+    X = Hdf5Dataset('tests/data/molecule.hdf5', 'X')
+    y = Hdf5Dataset('tests/data/molecule.hdf5', 'y')[:].ravel()
+
+    n_top = 50
+    y_top = np.argsort(y)[-n_top:]
+
+    # ----------------------------------------------------
+    X_train_ind = list(RAND.choice(len(X), size=10))
+    y_train = y[X_train_ind]
+
+    # ----------------------------------------------------
+    kms = KMeans(n_clusters=20, max_iter=5)
+    kms.fit(X[:])
+    X_cls = kms.cluster_centers_
+    cls_ind, _ = vq(X_cls, X[:])  # need to use in data locations
+
+    # ----------------------------------------------------
+    dense_model = DenseTanimotoModel(X=X, X_M=cls_ind)
+    model = SparseGaussianProcess(dense_model=dense_model)
+    acqu = GreedyNRanking()
+
+    for itr in range(10, 51):
+            
+        y_train = y[X_train_ind]
+
+        if itr % 10 == 0:
+            model.update_parameters(X_train_ind, y_train)
+        model.update_data(X_train_ind, y_train)
+
+        posterior = model.sample_y(n_samples=1)
+        _, _  = model.predict()
+        alpha = acqu.score_points(posterior)
+        alpha_ranked = np.argsort(alpha)[::-1]
+        to_sample = [i for i in alpha_ranked if i not in X_train_ind][0]
+        
+        X_train_ind.append(to_sample)    
+        n_top_found = sum([1 for i in X_train_ind if i in y_top])
+        
+        print(F'[{itr} / 50] : {n_top_found}/{n_top} found : y_max_tested={max(y):.3f} : y_sampled={y[to_sample]}')
+        
+        
+    assert n_top_found >= 17  # average with random guessing is 6.5
+   
 
 # -----------------------------------------------------------------------------------------------------------------------------
