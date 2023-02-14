c1a7b0a253c9b80d8e32762831dd8235de06ae6f
Bundaberg-Joey <calumhand94@hotmail.com>
2023-02-14 17:31:43 +0000

    implementing safe matern kernels as per gpflow issue 490

--- mkl/kernel.py
+++ mkl/kernel.py
@@ -3,6 +3,10 @@ from typing import Optional
 import gpflow
 from gpflow.utilities import positive, ops
 import tensorflow as tf
+from gpflow.kernels import Matern12, Matern32, Matern52
+
+
+# --------------------------------------------------------------------------------------------------------------------
 
 
 class TanimotoKernel(gpflow.kernels.Kernel):
@@ -63,3 +67,30 @@ class TanimotoKernel(gpflow.kernels.Kernel):
         tf.Tensor
         """
         return tf.fill(len(X), self.variance)
+    
+    
+# --------------------------------------------------------------------------------------------------------------------
+
+
+class ImplementSafeEuclidDistance:
+    # as per https://github.com/GPflow/GPflow/issues/490
+    # introduces small noise term to distance calcultion for very close vectors
+    
+    def euclid_dist(self, X, X2):
+        r2 = self.square_dist(X, X2)
+        return tf.sqrt(r2 + 1e-6)
+    
+
+class SafeMatern12(ImplementSafeEuclidDistance, Matern12):
+    pass
+
+
+class SafeMatern32(ImplementSafeEuclidDistance, Matern32):
+    pass
+
+
+class SafeMatern52(ImplementSafeEuclidDistance, Matern52):
+    pass
+
+
+# --------------------------------------------------------------------------------------------------------------------
